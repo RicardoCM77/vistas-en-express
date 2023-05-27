6be5aa62bdc496b0c2186780937aaef7ade83bf9
RicardoCM77 <ricardocortes479@gmail.com>
2023-05-27 13:01:45 -0600

    Modelo de datos

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
 // Importando Express
 import express from 'express';
+import httpStatus from 'http-status';
+// Se importa path
+import path from 'path';
+
+// Template Engine
+//import { engine } from 'express-handlebars';
 
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
 
+// Importando el directorio raiz
+import { ROOT_DIR } from './helpers/paths.js'
+
+
+
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
 
+// Se crea instancia del template engine
+//const hbsTemplateEngine = engine({
+  // Extensión de los archivos de plantillas
+  //extname: '.hbs',
+  // Nombre del diseño por defecto
+  //defaultLayout: 'main',
+//});
+
+// TE1. Se registra en la instancia de express
+//app.engine('hbs', hbsTemplateEngine);
+
+// TE2.Se selecciona el Template Engine
+//app.set('view engine', 'hbs');
+
+// TE3. Se establece la ruta de las vistas
+//app.set('views', path.resolve('views'));
+
 // Se registra el middleware del body-parser
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
+
+// Se registra el middleware para el servidor
+// de archivos estaticos
+app.use(express.urlencoded({extended: true }));
+app.use(express.static(path.join(ROOT_DIR, 'public')));
 
 // Se agrega ruta de administrador
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
 
+// Registrando el middleware para el error
+// 404
+app.use((req, res, next) => {
+  res.status(httpStatus.NOT_FOUND)
+  .sendFile(path.resolve('views','404.html'))
+});
 
 // Definiendo puertos
 const port = 3000;
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,22 +2,31 @@
 import { Router } from 'express';
 // Importando el gestor de rutas
 import path from 'path';
+//import {ROOT_DIR} from `'../helpers/paths:js':
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// GET /add-product
+
+// Datos en memoria volatil
+export const products = [];
+
+
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
   res.sendFile(path.resolve('views','add-product.html'));
 });
 
-// POST /add-product
+// POST /admin/add-product
 router.post('/add-product', (req, res) => {
-  // Realizaremos la extracción de
-  // parametros dentro de la peticion
-  console.log(req.body);
+  // Realizaremos la desestructuracion de
+  // "name" de la petición
+  const { title } = req.body;
+  // Agregamos el dato en la base de datos
+  products.push(title);
+  // Redireccionando
   res.redirect('/');
 });
 
